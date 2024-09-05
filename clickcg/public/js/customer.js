2b4e9419e92bf092c864832bbf5a486728dcc9c0
Ameer Muavia Shah <mavee.shah@hotmail.com>
2024-09-06 04:20:24 +0500

    feat: clear custom buttons and add so to override them without trouble

--- clickcg/public/js/customer.js
+++ clickcg/public/js/customer.js
@@ -1,9 +1,12 @@
 frappe.ui.form.on("Customer", {
     refresh: function (frm) {
         if (!frm.doc.__islocal) {
+            // Clear existing buttons
+            frm.clear_custom_buttons();
+
             // custom buttons
             frm.add_custom_button(
-                __("Cuentas por Cobrar"),
+                __("Accounts Receivable"),
                 function () {
                     frappe.set_route("query-report", "Cuentas por Cobrar", {
                         party_type: "Customer",
@@ -12,6 +15,49 @@ frappe.ui.form.on("Customer", {
                 },
                 __("View")
             );
+
+            frm.add_custom_button(
+                __("Accounting Ledger"),
+                function () {
+                    frappe.set_route("query-report", "General Ledger", {
+                        party_type: "Customer",
+                        party: frm.doc.name,
+                        party_name: frm.doc.customer_name,
+                    });
+                },
+                __("View")
+            );
+
+            frm.add_custom_button(
+                __("Pricing Rule"),
+                function () {
+                    erpnext.utils.make_pricing_rule(frm.doc.doctype, frm.doc.name);
+                },
+                __("Create")
+            );
+
+            frm.add_custom_button(
+                __("Get Customer Group Details"),
+                function () {
+                    frm.trigger("get_customer_group_details");
+                },
+                __("Actions")
+            );
+
+            if (cint(frappe.defaults.get_default("enable_common_party_accounting"))) {
+                frm.add_custom_button(
+                    __("Link with Supplier"),
+                    function () {
+                        frm.trigger("show_party_link_dialog");
+                    },
+                    __("Actions")
+                );
+            }
+
+            // indicator
+            erpnext.utils.set_party_dashboard_indicators(frm);
+        } else {
+            frappe.contacts.clear_address_and_contact(frm);
         }
     },
 });
